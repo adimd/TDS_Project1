222db856c6ca6b6137170d901a110fdd1241a418
adimd <adimd2397@gmail.com>
2025-02-16 16:06:56 +0530

    Commit

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ def read_file():
         return jsonify({"error": f"Internal server error: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
